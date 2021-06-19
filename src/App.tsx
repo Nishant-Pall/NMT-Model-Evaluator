9baa0cbf5e9a95fad7719a442be7fcafd4de8797
Nishant-Pall <palln6935@gmail.com>
2021-06-20 05:27:21 +0530

    Setup strucutre

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Heading">
+          Here you can evaluate or compare Machine Translation Models according to their BLEU Score
+        </div>
+        <div className="Main">
+          YO
+        </div>
       </header>
     </div>
   );
